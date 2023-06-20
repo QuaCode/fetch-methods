101b109a2153c00d237bd2c709f601aab5c6a565
Geovanny <geovannyalvarez504@gmail.com>
2023-06-20 14:38:07 -0600

    ✨ feat: Integrate HTTP Requests
    Usando Fetch crear un wraper que nos permita manejar los errores e insertar un token sin necesidad de estarlo haciendo manualmente para cada petición.

---
+++ src/api/fetch/api.ts
@@ -0,0 +1,76 @@
+import { Methods } from '@/types'
+
+const apiUrl = String(process.env.NEXT_PUBLIC_API)
+
+class ApiClient {
+ private accessToken: string | undefined
+
+ constructor() {}
+
+ private async fetchToken(): Promise<string> {
+  // Lógica para obtener el token de acceso (ejemplo: desde localStorage)
+  // const token = localStorage.getItem('accessToken')
+  // Necesita el 'use client' de NextJs para funcionar con localStorage
+
+  const token = 'accessToken'
+  if (token) {
+   return token
+  }
+
+  // Si el token no está en localStorage, realizar una llamada para obtenerlo
+  return new Promise<string>((resolve, reject) => {
+   setTimeout(() => {
+    const fetchedToken = 'exampleToken123'
+    resolve(fetchedToken)
+   }, 2000)
+  })
+ }
+
+ private async handleErrors(response: Response): Promise<Response> {
+  if (!response.ok) {
+   // Podríamos meter algún toast redireccionar a alguna página que maneje los errores y los muestre por pantalla.
+   console.log({ response })
+   throw new Error(`HTTP error, status code: ${response.status}`)
+  }
+  return response
+ }
+
+ private async fetch<T>({
+  body,
+  url,
+  method = 'GET',
+ }: {
+  url: string
+  body?: BodyInit
+  method?: Methods
+ }): Promise<T> {
+  const token = await this.fetchToken()
+  const headers = {
+   Authorization: `Bearer ${token}`,
+  }
+
+  console.log({ apiUrl })
+  const response = await fetch(`${apiUrl}${url}`, {
+   method,
+   headers,
+   body,
+  })
+
+  await this.handleErrors(response)
+  return response.json()
+ }
+
+ public async get<T>(url: string): Promise<T> {
+  return await this.fetch<T>({ url, method: 'GET' })
+ }
+
+ public async post<T>(url: string, body: any): Promise<T> {
+  return await this.fetch<T>({ url, method: 'POST', body })
+ }
+
+ public async delete<T>(url: string): Promise<T> {
+  return await this.fetch<T>({ url, method: 'DELETE' })
+ }
+}
+
+export const api = new ApiClient()
---
+++ src/hooks/useGetPokemon.ts
@@ -0,0 +1,35 @@
+'use client'
+import { api } from '@/api/fetch/api'
+import { ApiResponse } from '@/types/ApiResponse'
+import { useState, useEffect } from 'react'
+
+export const useGetPokemon = <T>(url: string): ApiResponse<T> => {
+ const [response, setResponse] = useState<ApiResponse<T>>({
+  data: null,
+  error: null,
+  isLoading: true,
+ })
+
+ useEffect(() => {
+  const fetchData = async () => {
+   try {
+    const result = await api.get<T>(url)
+    setResponse({
+     data: result,
+     error: null,
+     isLoading: false,
+    })
+   } catch (error) {
+    setResponse({
+     data: null,
+     error,
+     isLoading: false,
+    })
+   }
+  }
+
+  fetchData()
+ }, [url])
+
+ return response
+}
